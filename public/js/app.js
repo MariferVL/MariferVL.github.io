c4cc1eac63bd65fa74b48db438e06d0bacd3e384
Marifer Villalobos <mfvl.1186@gmail.com>
2023-08-28 01:41:25 -0400

    add projects, html and meta translations and change videos

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,15 @@ window.addEventListener('load', hideLoading);
 /* Language Switch */
 
 const translations = {
+  title: {
+    es: "Portafolio de Marifer - ¡Bienvenido a Mi Mundo!",
+    en: "Marifer's Portfolio - Welcome to My World!"
+  },
+  metaDescription: {
+    es: "Sitio web de Portafolio futurista y visualmente cautivador que muestra mi recorrido como desarrolladora fullstack. Explora mis proyectos, habilidades y visión creativa para el diseño web.",
+    en: "A futuristic and visually captivating portfolio website showcasing my journey as a fullstack developer. Explore my projects, skills, and creative vision for web design."
+  },
+  
   homeNav: {
     es: "Inicio",
     en: "Home"
@@ -115,7 +124,7 @@ const translations = {
     en: 'Server-side JavaScript.',
     es: 'JavaScript en el lado del servidor.'
   },
-  'express-description':  {
+  'express-description': {
     en: 'Node.js framework.',
     es: 'Framework de Node.js.'
   },
@@ -123,7 +132,7 @@ const translations = {
     en: 'Relational database system.',
     es: 'Sistema de base de datos relacional.'
   },
-  'java-description':  {
+  'java-description': {
     en: 'Object-oriented programming.',
     es: 'Programación orientada a objetos.'
   },
@@ -131,7 +140,7 @@ const translations = {
     en: 'Web service architecture.',
     es: 'Arquitectura de servicios web.'
   },
-  'git-description':  {
+  'git-description': {
     en: 'Version control.',
     es: 'Control de versiones.'
   },
@@ -139,7 +148,7 @@ const translations = {
     en: 'Code collaboration platform.',
     es: 'Plataforma de colaboración para código.'
   },
-  'trello-description':  {
+  'trello-description': {
     en: 'Project management tool.',
     es: 'Herramienta de gestión de proyectos.'
   },
@@ -151,7 +160,7 @@ const translations = {
     en: 'Project management methodology.',
     es: 'Metodología de gestión de proyectos.'
   },
-  'kanban-description':  {
+  'kanban-description': {
     en: 'Visual workflow management.',
     es: 'Gestión visual de flujo de trabajo.'
   },
@@ -159,22 +168,82 @@ const translations = {
     en: 'Agile framework.',
     es: 'Marco de trabajo ágil.'
   },
-
+  projectDescription1: {
+    en: "Social Network for Group Events.",
+    es: "Red Social para eventos grupales.",
+  },
+  projectDescription2: {
+    en: "Website about Vedic Astrology",
+    es: "Website sobre Astrología Védica",
+  },
+  projectDescription3: {
+    en: "Administrative Website for Vegetarian Restaurant",
+    es: "Website Administrativo para Restaurante Vegetariano",
+  },
+  projectDescription4: {
+    en: "NPM Package to analyze .md files",
+    es: "Paquete NPM para analizar archivos .md",
+  },
+  projectDescription5: {
+    en: "Website Simulation of Hites as a technical challenge.",
+    es: "Simulación Página Web Hites como reto técnico.",
+  },
+    projectTitle6: {
+      en: "Bank Card Validator",
+      es: "Validador Tarjeta Bancaria",
+    },
+    projectDescription6: {
+      en: "Template that validates the card number and its issuer.",
+      es: "Template que valida el número de la tarjeta y su empresa.",
+    },
+    projectDescription7: {
+      en: "Website for animal rescue foundation.",
+      es: "Website para fundación de rescate animal.",
+    },
+    projectDescription8: {
+      en: "Memory and motor skills game.",
+      es: "Juego de memoria y agilidad motriz.",
+    },
+    projectTitle9: {
+      en: "Naif Style Portfolio",
+      es: "Portafolio estilo Naif",
+    },
+    projectDescription9: {
+      en: "Portfolio for professionals with Naif style.",
+      es: "Portafolio para profesionales con estilo Naif.",
+    },
+    projectDescription10: {
+      en: "Sensorimotor game based on a drum set.",
+      es: "Juego sensoriomotor en base a un set de batería.",
+    },
+    projectTitle11: {
+      en: "Movie and Music Trivia",
+      es: "Trivia cine y música",
+    },
+    projectDescription11: {
+      en: "Pre-Admission project for Laboratoria.",
+      es: "Proyecto Pre-Admisión Laboratoria.",
+    },
 };
 
 
 document.getElementById("language-toggle").addEventListener("change", () => {
   const lang = document.getElementById("language-toggle").checked ? "en" : "es";
   updateContent(lang);
-  const video = document.querySelector("video");
-  const sourceElement = language === "en"
-    ? video.querySelector('[src="images/videos/videoEn.mp4"]')
-    : video.querySelector('[src="images/videos/videoEs.mp4"]');
+  
+  document.getElementById("html").setAttribute("lang", lang);
+  const metaDescriptionElement = document.querySelector('meta[class="websiteDescription"][data-translate="metaDescription"]');
+  const translatedDescription = translations["metaDescription"][lang];
+  metaDescriptionElement.setAttribute("content", translatedDescription);
+
+  const video = document.querySelector("video"); 
+  const sourceElement = lang === "es"
+    ? video.querySelector('[data-lang="es"]')
+    : video.querySelector('[data-lang="en"]');
+  
+  video.src = sourceElement.getAttribute("src");
   video.load();
-  video.pause();
-  video.currentTime = 0;
-  sourceElement.removeAttribute("hidden");
-  sourceElement.siblings().setAttribute("hidden", true);
+
 });
 
 function updateContent(lang) {
